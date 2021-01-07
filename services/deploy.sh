a8524b75b6162688d5f55123182239e2276beb04
Markus Weber <marweb@alice.de>
2021-01-07 20:05:40 +0100

    autostart for sensor measuring

---
+++ services/deploy.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# Copy the service into the systems services folder
+# Inject path to executable
+sensors_script=$(realpath ../sensors.py)
+sed -e "s/\${executable}/$sensors_script/" ${SCRIPTPATH}/mqtt-temp.service >> /etc/systemd/system/mqtt-sensors.service
