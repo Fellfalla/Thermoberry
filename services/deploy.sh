66b116c1b39aa9be88b6b2a2f5820ada5bbe6e09
Markus Weber <marweb@alice.de>
2021-01-07 20:09:07 +0100

    hoftix: need sudo to deploy service

--- services/deploy.sh
+++ services/deploy.sh
@@ -5,4 +5,4 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 # Copy the service into the systems services folder
 # Inject path to executable
 sensors_script=$(realpath ../sensors.py)
-sed -e "s/\${executable}/$sensors_script/" ${SCRIPTPATH}/mqtt-temp.service >> /etc/systemd/system/mqtt-sensors.service
+sudo sed -e "s/\${executable}/$sensors_script/" ${SCRIPTPATH}/mqtt-temp.service >> /etc/systemd/system/mqtt-sensors.service
